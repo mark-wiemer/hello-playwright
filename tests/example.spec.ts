deeb3fba86136c529004f0487d8b8847a0976d3c
Mark Wiemer <markwiemer@outlook.com>
2023-04-12 22:13:33 -0700

    Change tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,5 +4,15 @@ test("has title", async ({ page }) => {
   await page.goto("index.html");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("Example Domain");
+  await expect(page).toHaveTitle("Test page");
+});
+
+test("has what I want", async ({ page }) => {
+  await page.goto("index.html");
+
+  await expect(
+    page
+      .locator(`[aria-label="Main menu"]`)
+      .getByText("This is what you want.", { exact: true })
+  ).toBeVisible();
 });
